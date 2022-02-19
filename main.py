9cc585e58216fc5f9c374de8ea5aebfc3b576573
thenor <aldo.fiorito97@gmail.com>
2022-02-19 14:12:41 +0100

    trasform matrix

--- main.py
+++ main.py
@@ -16,6 +16,17 @@ def infoVideo(cap:Any):
     
     return n_frames
 
+def ComputeMatrix(good_old, good_new):
+    m = cv2.estimateAffine2D(good_old, good_new)[0]
+    #print("matrix",m)
+    dx = m[0,2]
+    dy = m[1,2]
+    da = np.arctan2(m[1,0], m[0,0])
+
+    return [dx,dy,da]
+
+
+
 #Lucas-Kanade Optical Flow
 def processFrames(cap:Any, frames:int,feature_params :dict,lk_parames:dict):
     #process the first frame for starting the sequence
@@ -23,28 +34,30 @@ def processFrames(cap:Any, frames:int,feature_params :dict,lk_parames:dict):
     prev_gray = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
     #mask previous frame to draw lines
     mask = np.zeros_like(prev_frame)
-
     # Create some random colors
-    color = np.random.randint(0, 255, (100, 3))
+    color = np.random.randint(0, 255, (150, 3))
+    transforms = np.zeros((frames-1, 3), np.float32)
     
     for i in range(0,frames-2):
         prevPtsFeat = cv2.goodFeaturesToTrack(prev_gray,**feature_params)
-        print("goodFT",prevPtsFeat)
+        #print("goodFT",prevPtsFeat)
         success, frame = cap.read() #true,prev
         
-        if ( not success):
+        if (not success):
             break
         
-        print("i#frame",i,"\n")
+        #print("i#frame",i,"\n")
         curr_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         #print(curr_gray)
-        cv2.imshow("image gray",curr_gray)
-        cv2.waitKey(0)
+        #cv2.imshow("image gray",curr_gray)
+        #cv2.waitKey(0)
         curr_pts, status, err = cv2.calcOpticalFlowPyrLK(prev_gray, curr_gray, prevPtsFeat, None,**lk_parames)
 
         if curr_pts is not None:
             good_new = curr_pts[status==1]
             good_old = prevPtsFeat[status==1]
+
+
         # Sanity check
         assert prevPtsFeat.shape == curr_pts.shape
 
@@ -52,11 +65,25 @@ def processFrames(cap:Any, frames:int,feature_params :dict,lk_parames:dict):
             a, b = new.ravel()
             c, d = old.ravel()
             mask = cv2.line(mask, (int(a), int(b)), (int(c), int(d)), color[i].tolist(), 2)
-            frame = cv2.circle(frame, (int(a), int(b)), 5, color[i].tolist(), -1)
-            
+            frame = cv2.circle(frame, (int(a), int(b)), 3, color[i].tolist(), -1)
+
         img = cv2.add(frame, mask)
         cv2.imshow('frame', img)
-        exit()
+
+
+        #Find transformation matrix
+        transforms[i]=ComputeMatrix(good_old,good_new)
+        print(transforms[i])
+    
+
+        #space for next iter, esc to quit
+        k = cv2.waitKey(0) & 0xff
+        if k == 27:
+            break
+
+        prev_gray = curr_gray.copy()
+        prevPtsFeat = good_new.reshape(-1, 1, 2)
+    cv2.destroyAllWindows()
  
 
 
@@ -64,9 +91,10 @@ def processFrames(cap:Any, frames:int,feature_params :dict,lk_parames:dict):
 
 if __name__ == '__main__':
     cap = cv2.VideoCapture('./gallery/test1.mp4')
+    print(cv2.__version__)
+    print("Getting info video \n")
     frames = infoVideo(cap)
-
-    feature_params = dict(maxCorners=200,qualityLevel=0.01,minDistance=30,blockSize=3)
+    feature_params = dict(maxCorners=100,qualityLevel=0.10,minDistance=40,blockSize=3)
     # Parameters for lucas kanade optical flow
     lk_params = dict(winSize  = (15, 15),maxLevel = 2,criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
     processFrames(cap,frames,feature_params,lk_params)
